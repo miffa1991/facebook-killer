4fe09638bc3f8e1a91c4e01eda22dcd24a627332
miffa1991 <mifa912088@gmail.com>
2020-02-21 22:34:28 +0200

    refactoring component, clear function

--- src/components/Content/ContentMain/Users/Users.jsx
+++ src/components/Content/ContentMain/Users/Users.jsx
@@ -1,102 +1,82 @@
 import React from 'react';
 import User from './User/User';
 import s from './Users.module.css';
-import * as axios from 'axios';
+
 import userPhoto from './../../../../assets/img/user.jpeg';
 
 
-class Users extends React.Component {
 
-  componentDidMount() {
-    axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.pageCurrent}&count=${this.props.pageLimit}`).then(response => {
-      this.props.setUsers(response.data.items);
-      this.props.setUserCount(response.data.totalCount);
-    });
-    //this.props.pageUserCount = totalCount;
+const Users = (props) => {
+  let pageCount = Math.ceil(props.pageUserCount / props.pageLimit);
+  let pages = [];
+  for (let i = 1; i < pageCount; i++) {
+    pages.push(i);
   }
-changeCurrentPage = (pageNumber) => {
-  this.props.currentPage(pageNumber);
-  axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageLimit}`).then(response => {
-    this.props.setUsers(response.data.items);
-    
-    
-  });
-}
-  render() {
-    let pageCount = Math.ceil(this.props.pageUserCount / this.props.pageLimit);
-    let pages = [];
-    for (let i = 1; i < pageCount; i++) {
-      pages.push(i);
-    }
-    //debugger;
-    return (
-      <div className={`${s['video-block']} ${s['section-padding']}`}>
+  return (
+    <div className={`${s['video-block']} ${s['section-padding']}`}>
       <div className={s.pagination}>
         {pages.map(p => {
-          return <div className={this.props.pageCurrent == p && s.activePage} onClick={() => { this.changeCurrentPage(p) }} >{p}</div>
+          return <div className={props.pageCurrent == p && s.activePage} onClick={() => { props.changeCurrentPage(p) }} >{p}</div>
         })}
-        </div>
-        <div className={s['main-items']}>
-          <div className="row justify-content-md-center">
-            <div className={`${s.messages} text-center col-md-12`}>
-              Find Profiles
+      </div>
+      <div className={s['main-items']}>
+        <div className="row justify-content-md-center">
+          <div className={`${s.messages} text-center col-md-12`}>
+            Find Profiles
             <hr />
-            </div>
-            <div className="feedback col-md-12">
-              <div className={s['search-people']}>
-                <input type="search" className={s['search-people-input']} />
-                <button className={s['people-search']}>Search</button>
-              </div>
+          </div>
+          <div className="feedback col-md-12">
+            <div className={s['search-people']}>
+              <input type="search" className={s['search-people-input']} />
+              <button className={s['people-search']}>Search</button>
             </div>
           </div>
-          {/* <button onClick={this.getSettetsUsers}>addUsers</button> */}
-          <div className={s['my-all-lisitngs']}>
-            <div className="row">
-              {this.props.users.map(u => <div key={u.id} className="col-lg-3 col-sm-12">
-                <div className={s['my-listing-dt-all-follow']}>
-                  <div className={s['my-lisiting-picy']}>
-                    <a href="#"> <img src={u.photos.small != null ? u.photos.small : userPhoto} alt="" /></a>
-                  </div>
-                  <a href="#"><h4>{u.name}</h4></a>
-                  <p> <i className="fas fa-map-marker-alt"></i>{'u.location.country'} </p>
-                  <div className={s['dashboard-main-dt-follow']}>
-                    <ul>
-                      <li>
-                        <div className={s['lisitng-dt2']}>
-                          <ins>15</ins>
-                          <span>Listings</span>
-                        </div>
-                      </li>
-                      <li>
-                        <div className={s['lisitng-dt2']}>
-                          <ins>150</ins>
-                          <span>Followers</span>
-                        </div>
-                      </li>
-                      <li>
-                        <div className={s['lisitng-dt2']}>
-                          <ins>265</ins>
-                          <span>Following</span>
-                        </div>
-                      </li>
-                    </ul>
-                  </div>
-                  {u.followed ? <button className={s['folow-btn']} onClick={() => this.props.unfollow(u.id)}>Follow</button> : <button className={s['folow-btn']} onClick={() => this.props.follow(u.id)} >Unfollow</button>}
+        </div>
+        {/* <button onClick={this.getSettetsUsers}>addUsers</button> */}
+        <div className={s['my-all-lisitngs']}>
+          <div className="row">
+            { props.users.map(u => <div key={u.id} className="col-lg-3 col-sm-12">
+              <div className={s['my-listing-dt-all-follow']}>
+                <div className={s['my-lisiting-picy']}>
+                  <a href="#"> <img src={u.photos.small != null ? u.photos.small : userPhoto} alt="" /></a>
                 </div>
-              </div>)}
-            </div>
+                <a href="#"><h4>{u.name}</h4></a>
+                <p> <i className="fas fa-map-marker-alt"></i>{'u.location.country'} </p>
+                <div className={s['dashboard-main-dt-follow']}>
+                  <ul>
+                    <li>
+                      <div className={s['lisitng-dt2']}>
+                        <ins>15</ins>
+                        <span>Listings</span>
+                      </div>
+                    </li>
+                    <li>
+                      <div className={s['lisitng-dt2']}>
+                        <ins>150</ins>
+                        <span>Followers</span>
+                      </div>
+                    </li>
+                    <li>
+                      <div className={s['lisitng-dt2']}>
+                        <ins>265</ins>
+                        <span>Following</span>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+                {u.followed ? <button className={s['folow-btn']} onClick={() => props.unfollow(u.id)}>Follow</button> : <button className={s['folow-btn']} onClick={() => props.follow(u.id)} >Unfollow</button>}
+              </div>
+            </div>)}
           </div>
-          <div className={s['view-spinner']}>
-            <div className={s['spinner-border']} >
-              <span className={s['sr-only']}>Loading...</span>
-            </div>
+        </div>
+        <div className={s['view-spinner']}>
+          <div className={s['spinner-border']} >
+            <span className={s['sr-only']}>Loading...</span>
           </div>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
-
-
 
 export default Users;
--- src/components/Content/ContentMain/Users/UsersContainer.jsx
+++ src/components/Content/ContentMain/Users/UsersContainer.jsx
@@ -2,13 +2,39 @@ import React from 'react';
 import { followAC, unFollowAC, usersAC, currentPageAC, userCountAC } from './../../../../redux/users-reducer';
 import Users from './Users';
 import { connect } from "react-redux";
+import * as axios from 'axios';
+
+
+class UsersAPIComponent extends React.Component {
+
+  componentDidMount() {
+    axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.pageCurrent}&count=${this.props.pageLimit}`).then(response => {
+      this.props.setUsers(response.data.items);
+      this.props.setUserCount(response.data.totalCount);
+    });
+    //this.props.pageUserCount = totalCount;
+  }
+  changeCurrentPage = (pageNumber) => {
+    this.props.currentPage(pageNumber);
+    axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageLimit}`).then(response => {
+      this.props.setUsers(response.data.items);
+
+
+    });
+  }
+  render() {
+    return <Users pageUserCount={this.props.pageUserCount} pageLimit={this.props.pageLimit} pageCurrent={this.props.pageCurrent} users={this.props.users} changeCurrentPage={this.changeCurrentPage} unfollow ={this.props.unfollow}
+      follow={this.props.follow}/>
+  }
+}
+
 
 let mapStateToProps = (state) => {
   return {
     users: state.usersPage.users, //usersPage - ключ редюсера який ми вказали в redux-store.js
     pageLimit: state.usersPage.pageLimit,
-    pageUserCount:state.usersPage.pageUserCount,
-    pageCurrent:state.usersPage.pageCurrent
+    pageUserCount: state.usersPage.pageUserCount,
+    pageCurrent: state.usersPage.pageCurrent
   }
 }
 
@@ -16,11 +42,12 @@ const mapDispatchToProps = (dispatch) => {
   return {
     follow: (userId) => { dispatch(followAC(userId)) },
     unfollow: (userId) => { dispatch(unFollowAC(userId)) },
-    setUsers: (users) => { dispatch(usersAC(users))},
-    currentPage: (pageCurrent) => {dispatch(currentPageAC(pageCurrent))},
+    setUsers: (users) => { dispatch(usersAC(users)) },
+    currentPage: (pageCurrent) => { dispatch(currentPageAC(pageCurrent)) },
     setUserCount: (pageUserCount) => { dispatch(userCountAC(pageUserCount)) }
   }
 }
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users);
+
+const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(UsersAPIComponent);
 
 export default UsersContainer;
